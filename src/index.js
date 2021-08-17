609d0d9df98660a63c259e2af48e3c9f6ae22340
indrico <indrico.jowensen@gmail.com>
2021-08-17 08:03:34 +0700

    Merapikan Folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './tailwind.css';
-import App from './App';
+import './CSS/tailwind.css';
+import App from './Components/App';
 
 ReactDOM.render(
   <React.StrictMode>
